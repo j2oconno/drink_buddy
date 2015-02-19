7daadba696b68d2d629aa4806095e74f9f3c8e73
Jason O'Connor <j2oconno@ucsd.edu>
2015-02-18 18:15:58 -0800

    fixed drinkController.view

--- routes/drinkController.js
+++ routes/drinkController.js
@@ -1,9 +1,40 @@
-var ingredient = require("../midmint_ing.json")
-var direction = require("../midmint_dir.json")
+var data = require("../midmint.json")
 
-exports.drink = function(req, res){
-	console.log(ingredient);
-	console.log(direction);
-	res.render('MidnightMint',ingredient,direction);
+exports.view = function(req, res){
+	var thismood = req.params.mood;
+	var moods = data["moods"];
+	for (i=0;i<moods.length; i++){
+		if (moods[i]["mood_name"] === thismood){
+		console.log("Got HERE! OMG!!")
+		var ind = i;
+	};
+	}
+	console.log(moods[ind]);
+	console.log(moods[ind]["recipes"]);
+	res.render('recipe',moods[ind]);
 }
 
+exports.drink = function(req, res){
+	var moods = data["moods"];
+	var thismood = req.params.mood;
+	var thisdrink = req.params.recipe;
+	console.log(moods[0]);	
+	//This loop finds the object with 'mood_name' = 'thismood'
+	//i.e. finds the object of our mood
+	for (i=0;i<moods.length; i++){
+		if (moods[i]["mood_name"] === thismood){
+		console.log("Got HERE! OMG!!")
+		var ind1 = i;
+	};
+	}
+	concole.log(moods[ind1]);
+	//This loop finds the object with the "name" = 'thisdrink'
+	//i.e. finds the object of our drink
+	for (ii=0;ii<moods[ind1]["recipes"].length; ii++){
+		if (moods[ind1]["recipes"]["name"] === thisdrink){
+		console.log("Got HERE AGAIN! OMG!!")
+		var ind2 = ii;
+	};
+	}
+	res.render('drink',moods[ind1]["recipes"][ind2]);
+}
